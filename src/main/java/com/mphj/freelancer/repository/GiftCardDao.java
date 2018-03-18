d1a3207989bbfc08f0306d4762cc18a02096e14f
mohsen <alimohammadi340@gmail.com>
2018-03-18 16:34:08 -0400

    Views optimized

--- src/main/java/com/mphj/freelancer/repository/GiftCardDao.java
+++ src/main/java/com/mphj/freelancer/repository/GiftCardDao.java
@@ -1,7 +1,11 @@
 package com.mphj.freelancer.repository;
 
 import com.mphj.freelancer.repository.models.GiftCard;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+
+import javax.persistence.Query;
+import java.util.List;
 
 public class GiftCardDao extends BaseDao<GiftCard> {
 
@@ -14,6 +18,39 @@ public class GiftCardDao extends BaseDao<GiftCard> {
             super.save(giftCard);
         } else {
             super.update(giftCard);
+        }
+    }
+
+
+    public List<GiftCard> getAll() {
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+
+        List<GiftCard> list = session.createQuery("FROM GiftCard").list();
+
+        session.getTransaction().commit();
+        return list;
+    }
+
+
+    public GiftCard findBySerial(String serial) {
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+
+        Query query = session.createQuery("FROM GiftCard G WHERE G.serial = :serial");
+        query.setParameter("serial", serial.trim());
+        GiftCard giftCard = (GiftCard) query.getSingleResult();
+
+        session.getTransaction().commit();
+        return giftCard;
+    }
+
+
+    public boolean existsBySerial(String serial) {
+        try {
+            return findBySerial(serial) != null;
+        } catch (Exception e) {
+            return false;
         }
     }
 }
